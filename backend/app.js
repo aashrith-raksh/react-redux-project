16f96c4dd69dbb287140018bca4160161432f4e1
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-06 18:23:22 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const eventRoutes = require('./routes/events');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
