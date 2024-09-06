39d54608918fafb8af10db15c5b11012434254a0
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-06 17:12:50 +0530

    added an error page for error element of all routes

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,28 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  let message = "An error occured";
+  let title = "Error";
+
+  if (error.status === 500) {
+    message = error.message;
+  }
+
+  if (error.status === 404) {
+    message = "Path doesn't exist";
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+};
+
+export default ErrorPage;
